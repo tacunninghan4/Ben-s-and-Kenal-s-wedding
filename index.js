f0c103f223ee7a88e737cfa478c16905030dd2e9
Todd <andrewcunningham770@gmail.com>
2021-07-22 16:54:04 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+function createParagraph(){
+    var wordOne = document.getElementById("wordOne").value;
+     var wordtwo = document.getElementById("wordTwo").value;
+     var wordThree = document.getElementById("wordThree").value;
+     var wordFour = document.getElementById("wordFour").value;
+     var wordFive = document.getElementById("wordFive").value;
+     var wordSix = document.getElementById("wordSix").value;
+     var wordSeven = document.getElementById("wordSeven").value;
+     var wordEight = document.getElementById("wordEight").value;
+     var wordNine = document.getElementById("wordNine").value;
+     var wordTen = document.getElementById("wordTen").value;
+     var wordEleven = document.getElementById("wordEleven").value;
+       var wordTweleve = document.getElementById("wordTweleve").value;
+
+
+var paragraph = " Ben and Kendal had "+ wordOne + " month to prepare for their wedding. While making plans Ben had lots of "+ wordTwo + " suggstions While Kendal\'s were more(blank). Ben wanted to wear a " + wordThree + " " + wordFour +" to the wedding but upon seeing Ben in his outfit Kendal " + wordFive + " and decided that a" + wordSix + " " + wordSeven + " would be better suited for the big day. Ben however did not get a say in what Kendal would be wearing on the big day but she know the" + wordEight + "" + wordNine + " " + wordTen + " that she decided on. All in all Ben\'s and Kenda\'s wedding planning was a(n) " + wordEleven + "sucess. They especially think that their favour " + wordTweleve + "hit with the guest"
+
+
+document.getElementById('answer').innerHTML = paragraph;
+}
+
+
+
+
+
+
+
+
+
+
+
+
